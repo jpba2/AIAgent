1d522b23d77a92bdc1229b9e4c391528149d6ccb
JPBA <jpb@a.com>
2025-11-18 08:19:21 +0000

    Gemini API set up

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os
+from dotenv import load_dotenv
+import sys
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+client = genai.Client(api_key=api_key)
+
 def main():
     print("Hello from aiagent!")
 
+
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(f"{response.text}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
