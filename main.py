3d9600dd1c2357c1346e2907cd821c0b0e75ce68
JPBA <jpb@a.com>
2025-11-18 08:30:48 +0000

    Prompt accepts from sys.argv

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from aiagent!")
+    try: 
+        response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=f"{sys.argv[1]}"
+        )
 
-
-
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-    print(f"{response.text}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"{response.text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
