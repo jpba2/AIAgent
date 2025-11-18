4894f799903d79d8c24266fdebf40e99146cdb7a
JPBA <jpb@a.com>
2025-11-18 08:43:25 +0000

    code tidy up

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import os
-from dotenv import load_dotenv
 import sys
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
+from dotenv import load_dotenv
 from google import genai
-client = genai.Client(api_key=api_key)
+from google.genai import types
+
+
+
 
 def main():
-    print("Hello from aiagent!")
-    try: 
-        response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=f"{sys.argv[1]}"
+    load_dotenv()
+    
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
         )
 
-        print(f"{response.text}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+        
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
