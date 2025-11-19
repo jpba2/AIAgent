ffeef578a59b0b79dcd6c05f4388e390392b2a5f
JPBA <jpb@a.com>
2025-11-19 06:56:33 +0000

    Add --verbose tag

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
     user_prompt = " ".join(args)
-
+ 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
@@ -30,11 +30,16 @@ def main():
         model="gemini-2.0-flash-001", contents=messages
         )
 
-        
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+    if "--verbose" in user_prompt:
+        print(f"User prompt: {user_prompt}")    
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+    else:
+        print("Response:")
+        print(response.text)
+
 
 if __name__ == "__main__":
     main()
